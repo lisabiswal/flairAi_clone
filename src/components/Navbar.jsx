e4863b0ac0c91752aca1a41d172fb238664e06c6
Monalisa Biswal <monalisaabiswall@gmail.com>
2025-08-19 18:14:43 +0530

    add animation to navbar button on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react"
 import logo from "../assets/favicon.svg"
 export default function Navbar() {
+  const [scroll,setScroll] = useState(false)
+  useEffect(()=>{
+    const handelScroll = ()=>{
+      if(window.scrollY > 0){
+        setScroll(true)
+      }else{
+        setScroll(false)
+      }
+    }
+    window.addEventListener("scroll",handelScroll)
+    return () => {
+      window.removeEventListener("scroll", handelScroll);
+    }
+  },[])
+
   return (
     <header>
       <nav>
@@ -15,7 +31,7 @@ export default function Navbar() {
           <li>Community Gallery</li>
           <li>Resources <i className="ri-arrow-down-wide-line"></i></li>
           <li>
-            <button>Try Flair AI for free</button>
+            <button style={{backgroundColor: scroll ? "#85cc16": "#161616", color: scroll ? "#161616": "" }}>Try Flair AI for free</button>
           </li>
         </ul>
       </nav>
